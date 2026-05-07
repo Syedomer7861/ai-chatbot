3f347b8be5fa0ba16664ba9ed8184ee6d2161d1a
Syedomer7861 <farrukhmehdi10@gmail.com>
2026-05-07 15:24:42 +0530

    feat: initialize production request listener with exported build modules

--- api/index.js
+++ api/index.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = "production";
 import { createRequestListener } from "@react-router/node";
 import * as build from "../build/server/index.js";
 
-export default createRequestListener({ ...build });
+console.log("CRITICAL: Build properties found:", Object.keys(build));
+
+export default createRequestListener(Object.assign({}, build));
