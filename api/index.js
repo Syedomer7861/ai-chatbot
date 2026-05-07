f6506d210e41369d94386bdf57578990d9206ba7
Syedomer7861 <farrukhmehdi10@gmail.com>
2026-05-07 12:17:36 +0530

    feat: add serverless request handler for React Router build integration

--- api/index.js
+++ api/index.js
@@ -1,4 +1,28 @@
 import { createRequestHandler } from "react-router";
 import * as build from "../build/server/index.js";
 
-export default createRequestHandler(build);
+const handleRequest = createRequestHandler(build);
+
+export default async function (req, res) {
+  const host = req.headers.host;
+  const protocol = req.headers["x-forwarded-proto"] || "https";
+  const url = new URL(req.url, `${protocol}://${host}`);
+
+  const request = new Request(url.href, {
+    method: req.method,
+    headers: req.headers,
+    body: req.method !== "GET" && req.method !== "HEAD" ? req : undefined,
+    // @ts-ignore
+    duplex: "half",
+  });
+
+  const response = await handleRequest(request);
+
+  res.status(response.status);
+  for (const [key, value] of response.headers.entries()) {
+    res.setHeader(key, value);
+  }
+  
+  const body = await response.text();
+  res.send(body);
+}
