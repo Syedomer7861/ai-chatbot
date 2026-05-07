a5a868945dc96b70e0e1a516fac87f67e865b843
Syedomer7861 <farrukhmehdi10@gmail.com>
2026-05-07 15:07:29 +0530

    ..

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
+process.env.NODE_ENV = "production";
 import { createRequestListener } from "@react-router/node";
 import * as build from "../build/server/index.js";
 
-export default createRequestListener(build.default || build);
+export default createRequestListener({ ...build });
